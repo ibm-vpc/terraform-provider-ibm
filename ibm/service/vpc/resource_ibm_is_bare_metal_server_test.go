a100e61f925cc323ad083b8b665cdd6347498eeb
Ujjwal Kumar <Ujjwal.Kumar1@ibm.com>
2023-03-03 10:48:40 +0530

    Update resource_ibm_is_bare_metal_server_test.go

--- ibm/service/vpc/resource_ibm_is_bare_metal_server_test.go
+++ ibm/service/vpc/resource_ibm_is_bare_metal_server_test.go
@@ -55,7 +55,7 @@ func TestAccIBMISBareMetalServer_testZ(t *testing.T) {
 ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQCKVmnMOlHKcZK8tpt3MP1lqOLAcqcJzhsvJcjscgVERRN7/9484SOBJ3HSKxxNG5JN8owAjy5f9yYwcUg+JaUVuytn5Pv3aeYROHGGg+5G346xaq3DAwX6Y5ykr2fvjObgncQBnuU5KHWCECO/4h8uWuwh/kfniXPVjFToc+gnkqA+3RKpAecZhFXwfalQ9mMuYGFxn+fwn8cYEApsJbsEmb0iJwPiZ5hjFC8wREuiTlhPHDgkBLOiycd20op2nXzDbHfCHInquEe/gYxEitALONxm0swBOwJZwlTDOB7C6y2dzlrtxr1L59m7pCkWI4EtTRLvleehBoj3u7jB4usR
 `)
 	sshname := fmt.Sprintf("tf-sshname-%d", acctest.RandIntRange(10, 100))
-	profileName := "2x64"
+	profileName := "mz2d-metal-2x64"
 
 	resource.Test(t, resource.TestCase{
 		PreCheck:     func() { acc.TestAccPreCheck(t) },
