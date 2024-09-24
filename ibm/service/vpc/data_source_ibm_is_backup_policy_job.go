aa6940359554b436d60bb495d854bbb84ffc9d0e
Ujjwal Kumar <ujjwal.kumar1@ibm.com>
2024-09-24 09:56:07 +0530

    Update data_source_ibm_is_backup_policy_job.go

--- ibm/service/vpc/data_source_ibm_is_backup_policy_job.go
+++ ibm/service/vpc/data_source_ibm_is_backup_policy_job.go
@@ -322,7 +322,7 @@ func DataSourceIBMIsBackupPolicyJob() *schema.Resource {
 func dataSourceIBMIsBackupPolicyJobRead(context context.Context, d *schema.ResourceData, meta interface{}) diag.Diagnostics {
 	vpcClient, err := vpcClient(meta)
 	if err != nil {
-		tfErr := flex.TerraformErrorf(err, fmt.Sprintf("vpcClient creation failed: %s", err.Error()), "ibm_cloud", "read")
+		tfErr := flex.TerraformErrorf(err, fmt.Sprintf("vpcClient creation failed: %s", err.Error()), "(Data) ibm_is_backup_policy_job", "read")
 		log.Printf("[DEBUG]\n%s", tfErr.GetDebugMessage())
 		return tfErr.GetDiag()
 	}
