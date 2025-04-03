b94a56f24a0b86c10a3f4d633324e19018242afa
shreyasms07 <shreyas.m.s@ibm.com>
2025-04-03 21:58:14 +0530

    resolved review comments

--- ibm/service/vpc/data_source_ibm_is_lb_listener_policy_test.go
+++ ibm/service/vpc/data_source_ibm_is_lb_listener_policy_test.go
@@ -22,7 +22,6 @@ func TestAccIBMIsLbListenerPolicyDataSourceBasic(t *testing.T) {
 	protocol := "http"
 	port := "8080"
 	action := "forward"
-	//action := []string{"forward", "forward_pool", "forward_to_listener"}
 
 	resource.Test(t, resource.TestCase{
 		PreCheck:  func() { acc.TestAccPreCheck(t) },
