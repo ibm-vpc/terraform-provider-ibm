e91ab9a3321287b4df7361957752d3de36fe226d
keerthigp <keerthi.gp@ibm.com>
2026-09-16 16:32:48 +0530

    add missing test

--- ibm/service/vpc/data_source_ibm_is_public_address_ranges_test.go
+++ ibm/service/vpc/data_source_ibm_is_public_address_ranges_test.go
@@ -50,3 +50,81 @@ func testAccCheckIBMIsPublicAddressRangesDataSourceConfigBasic(vpcName, name, ip
 	}
 `)
 }
+
+// TestAccIBMIsPublicAddressRangesDataSourceVNITarget verifies that when an IPv6 PAR targets
+// a VNI, the list data source correctly populates target.virtual_network_interface on that entry.
+func TestAccIBMIsPublicAddressRangesDataSourceVNITarget(t *testing.T) {
+	vpcName := fmt.Sprintf("tf-vpc-pars-ds-%d", acctest.RandIntRange(10, 100))
+	subnetName := fmt.Sprintf("tf-subnet-pars-ds-%d", acctest.RandIntRange(10, 100))
+	vniName := fmt.Sprintf("tf-vni-pars-ds-%d", acctest.RandIntRange(10, 100))
+	authCIDRName := fmt.Sprintf("tf-authcidr-pars-%d", acctest.RandIntRange(10, 100))
+	parName := fmt.Sprintf("tf-par-pars-ds-%d", acctest.RandIntRange(10, 100))
+
+	resource.Test(t, resource.TestCase{
+		PreCheck:  func() { acc.TestAccPreCheck(t) },
+		Providers: acc.TestAccProviders,
+		Steps: []resource.TestStep{
+			{
+				Config: testAccCheckIBMIsPublicAddressRangesDataSourceVNIConfig(vpcName, subnetName, vniName, authCIDRName, parName),
+				Check: resource.ComposeTestCheckFunc(
+					resource.TestCheckResourceAttrSet("data.ibm_is_public_address_ranges.pars_vni_ds", "public_address_ranges.#"),
+					// Verify the PAR resource itself carries the VNI target
+					resource.TestCheckResourceAttr("ibm_is_public_address_range.testacc_par", "ip_version", "ipv6"),
+					resource.TestCheckResourceAttr("ibm_is_public_address_range.testacc_par", "target.#", "1"),
+					resource.TestCheckResourceAttr("ibm_is_public_address_range.testacc_par", "target.0.virtual_network_interface.#", "1"),
+					resource.TestCheckResourceAttrSet("ibm_is_public_address_range.testacc_par", "target.0.virtual_network_interface.0.id"),
+					resource.TestCheckResourceAttrSet("ibm_is_public_address_range.testacc_par", "target.0.virtual_network_interface.0.crn"),
+					resource.TestCheckResourceAttrSet("ibm_is_public_address_range.testacc_par", "target.0.virtual_network_interface.0.href"),
+					resource.TestCheckResourceAttrSet("ibm_is_public_address_range.testacc_par", "target.0.virtual_network_interface.0.name"),
+					resource.TestCheckResourceAttr("ibm_is_public_address_range.testacc_par", "target.0.virtual_network_interface.0.resource_type", "virtual_network_interface"),
+					resource.TestCheckResourceAttr("ibm_is_public_address_range.testacc_par", "target.0.vpc.#", "0"),
+				),
+			},
+		},
+	})
+}
+
+func testAccCheckIBMIsPublicAddressRangesDataSourceVNIConfig(vpcName, subnetName, vniName, authCIDRName, parName string) string {
+	return fmt.Sprintf(`
+resource "ibm_is_vpc" "testacc_vpc" {
+  name = "%s"
+}
+
+resource "ibm_is_subnet" "testacc_subnet" {
+  name                     = "%s"
+  vpc                      = ibm_is_vpc.testacc_vpc.id
+  zone                     = "%s"
+  total_ipv4_address_count = 256
+}
+
+resource "ibm_is_virtual_network_interface" "testacc_vni" {
+  name   = "%s"
+  subnet = ibm_is_subnet.testacc_subnet.id
+}
+
+resource "ibm_is_public_address_range_authorized_cidr" "testacc_auth_cidr" {
+  name                  = "%s"
+  ip_version            = "ipv6"
+  availability_mode     = "zonal"
+  zone                  = "%s"
+  network_prefix_length = 64
+}
+
+resource "ibm_is_public_address_range" "testacc_par" {
+  name                  = "%s"
+  network_prefix_length = 112
+  authorized_cidr {
+    id = ibm_is_public_address_range_authorized_cidr.testacc_auth_cidr.id
+  }
+  target {
+    virtual_network_interface {
+      id = ibm_is_virtual_network_interface.testacc_vni.id
+    }
+  }
+}
+
+data "ibm_is_public_address_ranges" "pars_vni_ds" {
+  depends_on = [ibm_is_public_address_range.testacc_par]
+}
+`, vpcName, subnetName, acc.ISZoneName, vniName, authCIDRName, acc.ISZoneName, parName)
+}
